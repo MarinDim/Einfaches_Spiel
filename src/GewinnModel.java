96932e3d8788aff333dc63b04007ba6d0d80e021
[MarinDim] <[mdimoski@student.tgm.ac.at]>
2026-09-18 21:16:38 +0200

    Parameterloser Konstrukter, der den Startwert auf 30 setzt. berechneComputerZahl Klasse, die der Zufallszahl erstellt. berechneRunde Klasse, die bestimmt, ob der Spieler der richtigen Zahl erraten hat

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -1,8 +1,33 @@
+import java.util.Random;
+
 public class GewinnModel {
     private int gesamtPunkte;
     private int spielerZahl;
     private int computerZahl;
     private int runderErgebnis;
 
+    public GewinnModel(){
+        this.gesamtPunkte=30; //Startpunkte auf 30 setzen
+    }
 
+    public void berechneComputerZahl(){
+        Random zufall=new Random();
+        this.computerZahl=zufall.nextInt(9)+1; //Eine Zufallszahl zwischen 1 und 9 erstellen
+    }
+
+    public void berechneRunde(int spielerZahl){
+        this.spielerZahl=spielerZahl; //Setzt der spielerZahl auf der eingegebene Zahl
+        berechneComputerZahl(); //Erstellt eine Zahl für den Computer
+
+        int abstand=this.spielerZahl-this.computerZahl; //Berechnen, wie nah die Schätzung war
+        if(abstand==0){
+            runderErgebnis=20;
+        }
+        else if(abstand==1||abstand==-1){
+            runderErgebnis=5;
+        }
+        else{
+            runderErgebnis=-10;
+        }
+    }
 }
