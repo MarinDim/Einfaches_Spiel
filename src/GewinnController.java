2bcf70f682deee5c1348e5eecad8659df4c7d1e6
[MarinDim] <[mdimoski@student.tgm.ac.at]>
2026-09-20 19:58:33 +0200

    Eine Methode zum Überprüfen der Eingabe

--- src/GewinnController.java
+++ src/GewinnController.java
@@ -10,4 +10,21 @@ public class GewinnController {
         this.model=model;
         this.view=view;
     }
+
+    public void spielRunde(){
+        String eingabe=view.getSpielerField().getText();
+
+        try{
+            int eingabeZahl=Integer.parseInt(eingabe);
+            if(eingabeZahl<1||eingabeZahl>9){
+                JOptionPane.showMessageDialog(view, "Ein Zahl zwischen 1 und 9 eingeben!", "Falsche Eingabe", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            model.berechneRunde(eingabeZahl);
+        }
+        catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(view, "Ein Zahl zwischen 1 und 9 eingeben!", "Falsche Eingabe", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
