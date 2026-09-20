7621976f8cfe6dfd7591bd900329b225deb7a2e8
[MarinDim] <[mdimoski@student.tgm.ac.at]>
2026-09-20 19:31:58 +0200

    Einen Konstruktor und zwei Attribute, nämlich model und view erstellt

--- src/GewinnController.java
+++ src/GewinnController.java
@@ -3,4 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GewinnController {
+    private GewinnModel model;
+    private GewinnView view;
+
+    public GewinnController(GewinnModel model, GewinnView view){
+        this.model=model;
+        this.view=view;
+    }
 }
