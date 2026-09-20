ce5b1fa63c6bc90413a51429739e2606acdd2e72
[MarinDim] <[mdimoski@student.tgm.ac.at]>
2026-09-20 16:24:08 +0200

    2 JTextField Attribute erstellt und ein JButton Attribut für das Wiederholen des Spiels erstellt

--- src/GewinnView.java
+++ src/GewinnView.java
@@ -2,10 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GewinnView extends JFrame{
-    private JLabel RundenTitelLabel;
-    private JLabel PunkteTitelLabel;
-    private JLabel RundenLabel;
-    private JLabel PunkteLabel;
+    private JLabel rundenTitelLabel;
+    private JLabel punkteTitelLabel;
+    private JLabel rundenLabel;
+    private JLabel punkteLabel;
+
+    private JTextField spielerField;
+    private JTextField computerField;
+    private JButton button;
 
 
 }
