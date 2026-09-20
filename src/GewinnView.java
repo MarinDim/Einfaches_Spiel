fd67defb6d5b875e45e1147390789269d3a19f3e
[MarinDim] <[mdimoski@student.tgm.ac.at]>
2026-09-20 16:21:44 +0200

    Insgesatm 4 JLabel Attribute für Punktestand und Rundenergebnis erstellt

--- src/GewinnView.java
+++ src/GewinnView.java
@@ -2,4 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GewinnView extends JFrame{
+    private JLabel RundenTitelLabel;
+    private JLabel PunkteTitelLabel;
+    private JLabel RundenLabel;
+    private JLabel PunkteLabel;
+
+
 }
